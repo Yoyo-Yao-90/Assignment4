31f77a4538c77e675666bc15f00179085d4b8e08
Lishan Yao <200466746@student.georgianc.on.ca>
2020-07-29 04:02:52 -0400

    Importing project

---
+++ Data/AppDbMovieContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class AppDbMovieContext : DbContext
+    {
+        public AppDbMovieContext (DbContextOptions<AppDbMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication1.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Type { get; set; }
+        public string Poster { get; set; }
+    }
+}
